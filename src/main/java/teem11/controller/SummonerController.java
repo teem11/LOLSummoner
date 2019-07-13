cd830272780a6bdf3309e31d5ae086d162e098f3
LeeHyunJu <dlguswn7175@ajou.ac.kr>
2019-07-13 15:50:04 +0900

    SummonerController 클래스에 summonerName을 이용하여 Restful하게 id와 name을 가져오는 함수 작성, 임시로 테스트하기 위함

---
+++ src/main/java/teem11/controller/SummonerController.java
@@ -0,0 +1,22 @@
+package teem11.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+import teem11.domain.Summoner;
+import teem11.service.SummonerService;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/lol-summoner")
+public class SummonerController {
+
+    @Autowired
+    private SummonerService summonerService;
+
+    @CrossOrigin(origins = "*", allowedHeaders = "*")
+    @GetMapping("/by-summonerName/{summonerName}")
+    public Summoner getCurrentSummonerInfo(@PathVariable String summonerName) {
+        return summonerService.getCurrentSummonerIdBySummonerName(summonerName);
+    }
+}
