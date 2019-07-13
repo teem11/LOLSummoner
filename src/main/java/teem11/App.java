b3f0f6bde4f8cb6d1879628c4cf0cd9d2846138c
LeeHyunJu <dlguswn7175@ajou.ac.kr>
2019-07-13 16:03:31 +0900

    App class의 main함수에 spring을 연동시켰음

--- src/main/java/teem11/App.java
+++ src/main/java/teem11/App.java
@@ -1,13 +1,14 @@
 package teem11;
 
-/**
- * Hello world!
- *
- */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
+
+@SpringBootApplication
+@EnableScheduling
+public class App {
+
+    public static void main(String[] args) {
+        SpringApplication.run(App.class, args);
     }
 }
